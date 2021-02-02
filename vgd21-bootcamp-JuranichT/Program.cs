9a78ee39622cf2cfb45fec6e7af041105ea9b8c5
Thomas <1707235@mukilteo.wednet.edu>
2021-02-01 20:33:05 -0800

    Add project files.

---
+++ vgd21-bootcamp-JuranichT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vgd21_bootcamp_JuranichT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
